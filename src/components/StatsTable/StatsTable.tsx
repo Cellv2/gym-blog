c3e0e6b804de12b2122bebff02394298430d4650
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-10-16 10:22:23 +0100

    Pre tailwind commit

---
+++ src/components/StatsTable/StatsTable.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type StatsTableProps = {
+
+}
+
+const StatsTable = (props: StatsTableProps) => {
+    return (
+        <div>
+            This is the stats table
+        </div>
+    )
+}
+
+export default StatsTable
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
+import StatsTable from "../components/StatsTable/StatsTable";
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
+            <StatsTable />
             <p>I'm making this by following the Gatsby Tutorial.</p>
             <StaticImage alt="Not Rashid" src="../images/karin.png" />
         </Layout>
